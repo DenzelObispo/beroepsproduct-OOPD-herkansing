b27c4738735b1f574a533f2400eb913c507b19ff
Denzel Obispo <58168241+DenzelObispo@users.noreply.github.com>
2023-04-22 13:42:56 +0200

    Changed enemy class to sprite entity

--- Boss Rush 2/src/main/java/com/github/hanyaeger/BossRush/entities/enemies/Enemy.java
+++ Boss Rush 2/src/main/java/com/github/hanyaeger/BossRush/entities/enemies/Enemy.java
@@ -2,18 +2,22 @@ package com.github.hanyaeger.BossRush.entities.enemies;
 
 import com.github.hanyaeger.BossRush.entities.Weapon.PlayerBullet;
 import com.github.hanyaeger.api.Coordinate2D;
+import com.github.hanyaeger.api.Size;
 import com.github.hanyaeger.api.UpdateExposer;
 import com.github.hanyaeger.api.entities.*;
+import com.github.hanyaeger.api.entities.impl.DynamicSpriteEntity;
 import com.github.hanyaeger.api.scenes.SceneBorder;
 
-public abstract class Enemy extends DynamicCompositeEntity implements Collider, Collided, UpdateExposer, SceneBorderTouchingWatcher {
+public abstract class Enemy extends DynamicSpriteEntity implements Collider, Collided, UpdateExposer, SceneBorderTouchingWatcher {
     public int health;
     public int moveSpeed;
-    public Enemy(Coordinate2D initialLocation, int health, int moveSpeed) {
-        super(initialLocation);
+    public String sprite;
+    public Enemy(Coordinate2D initialLocation, int health, int moveSpeed, String sprite) {
+        super(sprite,initialLocation, new Size(96));
 
         this.health = health;
         this.moveSpeed = moveSpeed;
+        this.sprite = sprite;
     }
 
     @Override
@@ -41,7 +45,6 @@ public abstract class Enemy extends DynamicCompositeEntity implements Collider, 
 
     @Override
     public void notifyBoundaryTouching(SceneBorder border){
-
         switch(border){
             case LEFT:
                 setMotion(moveSpeed, Direction.RIGHT);
--- Boss Rush 2/src/main/java/com/github/hanyaeger/BossRush/entities/enemies/Tentacle.java
+++ Boss Rush 2/src/main/java/com/github/hanyaeger/BossRush/entities/enemies/Tentacle.java
@@ -1,22 +1,12 @@
 package com.github.hanyaeger.BossRush.entities.enemies;
 
-import com.github.hanyaeger.BossRush.entities.sprites.TentacleSprite;
-import com.github.hanyaeger.BossRush.scenes.GameLevels;
 import com.github.hanyaeger.api.Coordinate2D;
 import com.github.hanyaeger.api.entities.Direction;
 
 public class Tentacle extends Enemy {
     public Tentacle(Coordinate2D initialLocation) {
-        super(initialLocation, 1000, 2);
+        super(initialLocation, 1000, 2, "sprites/tentacle.png");
 
         setMotion(moveSpeed, Direction.LEFT);
-    }
-
-    @Override
-    protected void setupEntities() {
-        GameLevels.currentLevel = 1;
-
-        var tentacleSprite = new TentacleSprite(new Coordinate2D(0,0));
-        addEntity(tentacleSprite);
     }
 }
